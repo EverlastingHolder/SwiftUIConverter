0d53e6664c179006663d709b7e22c6e2d93eee09
EverlastingHolder <moshkovcevroma@gmail.com>
2021-06-16 12:58:17 +0700

    Initial Commit

---
+++ ConverterRate/ConverterRateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterRateApp.swift
+//  ConverterRate
+//
+//  Created by Роман Мошковцев on 16.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct ConverterRateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
